d60c8a91b9368db5de93fe291560932e96ce676d
Hrithika753 <85736309+Hrithika753@users.noreply.github.com>
2024-05-14 13:35:36 +0530

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,17 @@
+import product1 from './assets/pan.png';
+import product2 from './assets/pcmx.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "PANADOL",
+        price: 60,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "paracetamol",
+        price: 45,
+        productImage: product2,
+    },
+];
